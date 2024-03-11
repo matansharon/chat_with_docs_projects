26e78cafaa384e938ad2b057c5341fb2464ffd53
matansharon <matca2953@gmail.com>
2024-03-11 22:22:58 +0200

    debugging alot

--- AI_Apps/chat_with_txt/helper_functions.py
+++ AI_Apps/chat_with_txt/helper_functions.py
@@ -7,20 +7,28 @@ from langchain_community.vectorstores.chroma import Chroma
 from langchain_core.prompts import ChatPromptTemplate
 from datetime import datetime
 import json
+
+
+
 load_dotenv()
 DIRECTORY_PATH='/Users/matansharon/python/chat_with_docs/data/text'
 DOCS_PATH='/Users/matansharon/python/chat_with_docs/AI_Apps/chat_with_txt/docs.json'
+
+
+
 def get_all_docs():
     
     with open(DOCS_PATH,'r') as f:
         data=json.load(f)
         docs=data['documents']
     return docs
+
 def load_all_docs_in_data_folder():
     loader = DirectoryLoader(DIRECTORY_PATH)
     documents = loader.load()
     return documents
-def load_and_split_documents(document):
+
+def split_document(document):
     
     
     text_splitter = RecursiveCharacterTextSplitter(
@@ -31,6 +39,7 @@ def load_and_split_documents(document):
     )
     chunks=text_splitter.split_documents(document)
     return chunks
+
 def create_new_db(chunks):
     path='chroma_db'
     if not os.path.exists(path):
@@ -38,9 +47,11 @@ def create_new_db(chunks):
         db=Chroma.from_documents(documents=chunks,embedding=OpenAIEmbeddings(),persist_directory=path)
         return db
     return load_db()
+
 def load_db():
     db = Chroma(persist_directory="chroma_db",embedding_function=OpenAIEmbeddings())
     return db
+
 def get_results_with_scores(query,db):
     bar=0.5
     res=db.similarity_search_with_relevance_scores(query,k=3)
@@ -60,16 +71,24 @@ def get_prompt_template(results,query):
     prompt_tamplate=ChatPromptTemplate.from_template(template)
     res=prompt_tamplate.format(context=temp,query=query)
     return res
+
 def get_response(query,db,model):
     results=get_results_with_scores(query,db)
     prompt_template=get_prompt_template(results,query)
     response=model.invoke(prompt_template)
     return response.content
+
 def main_app():
     # pass
-    chunks=load_and_split_documents()
-    db=create_new_db(chunks)
+    documtnts=load_all_docs_in_data_folder()
+    splited_documents=[]
+    for doc in documtnts:
+        d=split_document(doc)
+        split_document.append(split_document(d))
+        print(d)
+    print(splited_documents)
+    # db=create_new_db(documtnts)
     #for GPT-4 use this: 'gpt-4-turbo-preview'
-    model=ChatOpenAI()
+    # model=ChatOpenAI()
     
-    return db,model
+    # return db,model
