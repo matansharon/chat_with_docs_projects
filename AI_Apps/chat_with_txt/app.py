beb937efe1dfe7d32fc7297a79ee69d9d656cd8b
matansharon <matca2953@gmail.com>
2024-03-11 13:29:13 +0200

    in the middle of debugging

--- AI_Apps/chat_with_txt/app.py
+++ AI_Apps/chat_with_txt/app.py
@@ -1,20 +1,24 @@
 import streamlit as st
-from utils import *
-
+import helper_functions as hf
 
 
 def main():
-    st.header("Chat with me")
+    # st.header("Chat with me")
+    chunks=hf.load_and_split_documents()
+    db=hf.create_db(chunks)
+    model=hf.ChatOpenAI()
+    # st.write("I am ready to chat")
     
-    db,model=main_app()
-    st.write('Ask me anything')
-    query=st.text_input('Your question')
-    if query:
-        response=get_response(query,db,model)
-        if response=='No results found':
-            st.write('No results found')
-        else:
-            st.write(response)
+    
+    
+    query="what is qlora?"
+    results=hf.get_results_with_scores(query,db)
+    prompt_template=hf.get_prompt_template(results,query)
+    st.write(prompt_template)
+    # st.write(results[0][0].page_content)
+    # st.write(db.similarity_search(query))
+    
+    
     
     
 if __name__ == "__main__":
---
+++ AI_Apps/chat_with_txt/helper_functions.py
@@ -0,0 +1,65 @@
+from dotenv import load_dotenv
+import os
+from langchain_community.document_loaders import DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_community.vectorstores.chroma import Chroma
+from langchain_core.prompts import ChatPromptTemplate
+from datetime import datetime
+
+load_dotenv()
+directory_path='/Users/matansharon/python/chat_with_docs/data/text'
+
+def load_and_split_documents():
+    loader = DirectoryLoader(directory_path)
+    documents = loader.load()
+    
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=100,
+        length_function=len,
+        add_start_index=True
+    )
+    chunks=text_splitter.split_documents(documents)
+    return chunks
+def create_db(chunks):
+    path='chroma_db'
+    if not os.path.exists(path):
+        
+        db=Chroma.from_documents(documents=chunks,embedding=OpenAIEmbeddings(),persist_directory=path)
+        return db
+    return load_db()
+def load_db():
+    db = Chroma(persist_directory="chroma_db",embedding_function=OpenAIEmbeddings())
+    return db
+def get_results_with_scores(query,db):
+    bar=0.5
+    res=db.similarity_search_with_relevance_scores(query,k=3)
+    
+    return res
+def get_prompt_template(results,query):
+    template="""
+    answer the question base only on the following context:
+    {context}
+    answer the question base on the above context: {query}
+    
+    """
+    context_texts = []
+    for i in range(len(results)):
+        context_texts.append(results[i][0].page_content)
+    temp = "\n\n---\n\n".join(context_texts)
+    prompt_tamplate=ChatPromptTemplate.from_template(template)
+    res=prompt_tamplate.format(context=temp,query=query)
+    return res
+def get_response(query,db,model):
+    results=get_results_with_scores(query,db)
+    prompt_template=get_prompt_template(results,query)
+    response=model.invoke(prompt_template)
+    return response
+def main_app():
+    pass
+    # chunks=load_and_split_documents()
+    # db=create_db(chunks)
+    
+    # model=ChatOpenAI()
+    # return db,model
