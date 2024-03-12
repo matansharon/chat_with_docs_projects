14188ca47e17b65f936664a89fa76693e0881f7a
matansharon <matca2953@gmail.com>
2024-03-12 13:09:55 +0200

    in prodoction managed to load the file

--- AI_Apps/chat_with_txt/app.py
+++ AI_Apps/chat_with_txt/app.py
@@ -1,15 +1,17 @@
 import streamlit as st
 from helper_functions import *
-
+from PyPDF2 import PdfReader
 
 def main():
-    db,model=main_app()
+    # db,model=main_app()
     query=st.text_input("Ask me anything")
-    if query:
-        response=get_response(query,db,model)
-        st.write(response)
+    # if query:
+    #     # response=get_response(query,db,model)
+    #     st.write(response)
     file=st.file_uploader("Upload file")
     if file:
         st.write(file)
+        pdf=PdfReader(file).pages[0].extract_text()
+        st.write(pdf)
 if __name__ == "__main__":
     main()
