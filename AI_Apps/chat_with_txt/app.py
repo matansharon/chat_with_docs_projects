e6217e648ead12978d13cc50d3a1e74d73ea6940
matansharon <matca2953@gmail.com>
2024-03-14 07:25:10 +0200

    finish for goooooooooooooood

--- AI_Apps/chat_with_txt/app.py
+++ AI_Apps/chat_with_txt/app.py
@@ -2,12 +2,17 @@ import streamlit as st
 import helper_functions as hf
 from PyPDF2 import PdfReader
 
-def add_document_to_side_bar(doc_name):
+def add_document_to_side_bar():
     with st.sidebar:
-        st.markdown(f"- {doc_name}")
+            st.write("### this is the files that in the database:")
+            if not(st.session_state["documents_names"]==None):
+                for i in st.session_state.documents_names:
+                    st.markdown(f"- {i}")
+    print(st.session_state.documents_names)
+    
 
 def main():
-    if not("db" in st.session_state):
+    if ("db" not in st.session_state):
         st.session_state['db']=None
         st.session_state['model']=None
         st.session_state['documents_names']=None
@@ -16,21 +21,21 @@ def main():
         st.session_state['db']=db
         st.session_state['model']=model
         st.session_state['documents_names']=documents_names
+        add_document_to_side_bar()
         
-    # documents_names=["dskfnsdf","dsfsadf"]
-    with st.sidebar:
-        st.write("### this is the files that in the database:")
-        if not(st.session_state["documents_names"]==None):
-            for i in st.session_state.documents_names:
-                st.markdown(f"- {i}")
+        
+        
+    
+    
     pdf = st.file_uploader("Upload a .pdf file", type=["pdf"])
-    if pdf and not('db' in st.session_state):
+    if pdf and ('db' in st.session_state):
         st.write("pdf file were uploaded")
-        st.session_state.documents_names.append(pdf.name)
+
         
         p=PdfReader(pdf)
-        add_document_to_side_bar(pdf.name)
-        hf.add_document(p,db)
+        st.session_state.documents_names.append(pdf.name)
+        add_document_to_side_bar()
+        hf.add_document(p,st.session_state.db)
         pdf=None
     query=st.text_input("Ask me Anything about the documents")
     if query and not(st.session_state.db==None):
