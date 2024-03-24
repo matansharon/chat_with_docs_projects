a92d2a90343b3cdca7d2b088ffce14a037f15aa5
matansharon <matca2953@gmail.com>
2024-03-24 09:16:10 +0200

    starting og the project:

---
+++ AI_Apps/chat_with_db/app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import os
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores.chroma import Chroma
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.messages import AIMessage, HumanMessage, ChatMessage
+from langchain_community.vectorstores.chroma import Chroma
+
+load_dotenv()
+
+st.title('Chat with Database')
+st.header('This app is designed to help you find information in your Database')
+st.chat_input("Ask me Anything about the Database")
