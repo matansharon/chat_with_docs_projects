1c83f82af9800cb91d301ef49e6acb1ccfbca9fa
matansharon <matca2953@gmail.com>
2024-03-27 17:14:53 +0200

    veryyyyy good place to stop

--- AI_Apps/chat_with_audio/app.py
+++ AI_Apps/chat_with_audio/app.py
@@ -16,6 +16,17 @@ with st.sidebar:
     file=st.file_uploader("Upload your audio file", type=["mp3", "wav", "ogg"], key="audio")
     if file:
         st.session_state.audio_file=file
+    if st.button("Show summary") and 'transcription' in st.session_state:
+        audio_file = open(st.session_state.audio_file.name, "rb")
+        complition=st.session_state.client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "the following is a transcription. Please summarize it."},
+            {"role": "user", "content": st.session_state.transcription},
+        ]
+        )
+        st.write("here is a summerztion of the audio file: ")
+        st.write(complition.choices[0].message.content)
 if 'audio_file' in st.session_state:
     audio_file = open(st.session_state.audio_file.name, "rb")
     transcription = st.session_state.client.audio.transcriptions.create(
@@ -23,4 +34,14 @@ if 'audio_file' in st.session_state:
     file=audio_file, 
     response_format="text"
     )
-    st.write(transcription)
+    st.session_state.transcription=transcription
+user_query=st.chat_input("Type something...")
+if user_query:
+    response = st.session_state.client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant. base your answers on the following transcription: "+st.session_state.transcription},
+        {"role": "user", "content": user_query},
+    ]
+    )
+    st.write(response.choices[0].message.content)
