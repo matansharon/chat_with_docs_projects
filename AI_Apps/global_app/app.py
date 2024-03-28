c7d33c15113b7c687c35c129199a3459eba475ba
matansharon <matca2953@gmail.com>
2024-03-28 17:09:04 +0200

    ui finish

--- AI_Apps/global_app/app.py
+++ AI_Apps/global_app/app.py
@@ -24,10 +24,27 @@ from pandasai import SmartDataframe,SmartDatalake
 load_dotenv()
 
 def main():
+    # UI stage
     st.set_page_config(page_title="Global AI Apps",page_icon="🌍",layout="wide")
     st.title("Global AI Apps")
     st.write("This app is designed to help you navigate between the different AI apps")
     st.write("Please select the app you want to use from the sidebar")
+    
+    #init stage
+    if 'init' not in st.session_state:
+        st.session_state['init']=True
+        st.session_state['llm']=ChatOpenAI()
+        # st.session_state['llm']=ChatOpenAI(model_name='gpt-4-turbo-preview')
+        st.session_state['pandas_llm']=OpenAI()
+        st.session_state['pdf_file']=None
+        st.session_state['csv_file']=None
+        st.session_state['audio_file']=None
+        st.session_state['app']=None
+        st.session_state['chat_history']=[
+            AIMessage("Hello and welcome to the Global AI Apps"),
+        ]
+    
+    #sidebar stage
     with st.sidebar:
         file=st.file_uploader("Upload a .pdf file", type=["pdf","csv",'mp3'])
         if file:
@@ -37,23 +54,26 @@ def main():
         app=st.selectbox("Select the app you want to use",["Chat with PDF","Chat with Audio",'Chat with CSV'])
         if app:
             st.write(app)
-    if 'file' in st.session_state:
-        st.write(file.type)
     
-    llm=ChatOpenAI(model_name='gpt-4-turbo-preview')
-    # st.write(llm)
+    
+
+    #interaction stage
+
     user_input=st.chat_input("Ask me Anything about the documents")
     if user_input:
-        # st.write(f"You: {user_input}")
-        with st.chat_message("Human"):
-                    st.write(f"AI: {user_input}")
-        response=llm.invoke(f"base your answer on the following input {user_input}")
-        # st.write(f"AI: {response.content}")
-        with st.chat_message("AI"):
-                    st.write(f"AI: {response.content}")
+        response=st.session_state.llm.invoke(f"base your answer on the following input {user_input}")
+        st.session_state.chat_history.append(HumanMessage(user_input))
+        st.session_state.chat_history.append(AIMessage(response.content))
         
     
-    
+    #display the chat history
+    for message in st.session_state.chat_history:
+        if isinstance(message,HumanMessage):
+            with st.chat_message("Human"):
+                st.write(f"Human: {message.content}")
+        else:
+            with st.chat_message("AI"):
+                st.write(f"AI: {message.content}")
         
 if __name__=="__main__":
     main()
