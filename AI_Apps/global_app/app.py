bd8b8f6f04a8720156251cc0c2734115445a0372
matansharon <matca2953@gmail.com>
2024-03-28 19:51:42 +0200

    good place to stop

--- AI_Apps/global_app/app.py
+++ AI_Apps/global_app/app.py
@@ -23,68 +23,49 @@ from dotenv import load_dotenv
 from pandasai import SmartDataframe,SmartDatalake
 load_dotenv()
 
-def main():
+
+
+def init():
+    st.session_state['init']=True
+    # st.session_state['llm']=ChatOpenAI()
+    st.session_state['llm']=ChatOpenAI(model_name='gpt-4-turbo-preview')
+    st.session_state['pandas_llm']=OpenAI()
+    st.session_state['pdf_file']=None
+    st.session_state['csv_file']=None
+    st.session_state['audio_file']=None
+    st.session_state['app']='Chat with PDF'
+    st.session_state['chat_history']=[
+        AIMessage("Hello and welcome to the Global AI Apps"),
+    ]
+
+
+
+def Write_UI():
     # UI stage
-    st.set_page_config(page_title="Global AI Apps",page_icon="🌍",layout="wide")
-    st.title("Global AI Apps")
-    st.write("This app is designed to help you navigate between the different AI apps")
-    st.write("Please select the app you want to use from the sidebar")
+    if st.session_state.app=="Chat with PDF":
+        st.title("Chat with PDF's 📚")
+    elif st.session_state.app=="Chat with Audio":
+        st.title("Chat with Audio's 🎧")
+    elif st.session_state.app=="Chat with CSV":
+        st.title("Chat with CSV's 📊")
     
-    #init stage
-    if 'init' not in st.session_state:
-        st.session_state['init']=True
-        st.session_state['llm']=ChatOpenAI()
-        # st.session_state['llm']=ChatOpenAI(model_name='gpt-4-turbo-preview')
-        st.session_state['pandas_llm']=OpenAI()
-        st.session_state['pdf_file']=None
-        st.session_state['csv_file']=None
-        st.session_state['audio_file']=None
-        st.session_state['app']=None
-        st.session_state['chat_history']=[
-            AIMessage("Hello and welcome to the Global AI Apps"),
-        ]
-    
-    #sidebar stage
+    st.subheader("This app is designed to help you navigate between the different AI apps")
+    st.write("#### Please select the app you want to use from the sidebar")
     with st.sidebar:
-        file=st.file_uploader("Upload a .pdf file", type=["pdf","csv",'mp3'])
+        file=st.file_uploader("Upload a file", type=["pdf","csv",'mp3'])
         if file:
             if file.type=="application/pdf":
-                st.session_state['pdf_file']=file
+                st.session_state['pdf_file']=handel_pdf(file)
+
+
             elif file.type=="text/csv":
                 st.session_state['csv_file']=file
             elif file.type=="audio/mpeg":
                 st.session_state['audio_file']=file
-                
-                
-            
-            
-        #add a dropdown menu to select the app
-        st.session_state.app=st.selectbox("Select the app you want to use",["Chat with PDF","Chat with Audio",'Chat with CSV'])
-    if st.session_state.app:
-        if st.session_state.app=="Chat with PDF" and st.session_state.pdf_file:
-            # text=PyPDF2.PdfReader(st.session_state.pdf_file)
-            st.write(st.session_state.pdf_file)
-            pages=PyPDF2.PdfReader(st.session_state.pdf_file).pages
-            text=""
-            for page in pages:
-                text+=page.extract_text()
-            st.write('len of the uploaded text is: ',len(text))
-            
-            
-    
-    
-
-    #interaction stage
-
-    user_input=st.chat_input("Ask me Anything about the documents")
-    if user_input:
+        st.session_state.app=st.selectbox("Select the app you want to use",["Chat with PDF","Chat with Audio",'Chat with CSV'],)
+    display_chat_history()
         
-        response=st.session_state.llm.invoke(f"base your answer on the following input {user_input}")
-        st.session_state.chat_history.append(HumanMessage(user_input))
-        st.session_state.chat_history.append(AIMessage(response.content))
-        
-    
-    #display the chat history
+def display_chat_history():
     for message in st.session_state.chat_history:
         if isinstance(message,HumanMessage):
             with st.chat_message("Human"):
@@ -92,6 +73,57 @@ def main():
         else:
             with st.chat_message("AI"):
                 st.write(f"AI: {message.content}")
+
+def handel_pdf(file):
+    if st.session_state.app:
+            if st.session_state.app=="Chat with PDF":
+                # text=PyPDF2.PdfReader(st.session_state.pdf_file)
+                
+                pages=PyPDF2.PdfReader(file).pages
+                text=""
+                for page in pages:
+                    text+=page.extract_text()
+                
+                st.write('len of the uploaded text is: ',len(text))
+                return text
+def handel_audio():
+    pass
+def handel_csv():
+    pass
+
+def get_response(query):
+    if st.session_state.app=="Chat with PDF":
+        response=st.session_state.llm.invoke(f"base your answer on the following context {st.session_state['pdf_file']} and answer the following query: {query}")
+
+    return response.content
+
+
+
+
+def main():
+    st.set_page_config(page_title="ALL AI Apps",page_icon="🌍",layout="wide")
+    #init stage
+    if 'init' not in st.session_state:
+        init()
+    Write_UI()
+
+
+    #interaction stage
+
+    user_input=st.chat_input("Ask me Anything...")
+    if user_input:
+        if st.session_state.app=="Chat with PDF":
+            response=get_response(user_input)
+            st.session_state.chat_history.append(HumanMessage(user_input))
+            st.session_state.chat_history.append(AIMessage(response))
+            # st.write(get_response(user_input))
+            display_chat_history()
+            
+            
+        
+    
+    #display the chat history
+    
         
 if __name__=="__main__":
     main()
