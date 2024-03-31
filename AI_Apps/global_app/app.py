4209e40a0d9cfe37be078633f41aa0b91583c3d6
matansharon <matca2953@gmail.com>
2024-03-31 12:08:41 +0300

    finish with the website part

--- AI_Apps/global_app/app.py
+++ AI_Apps/global_app/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import os
 from dotenv import load_dotenv
-
+from langchain_community.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores.chroma import Chroma
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
@@ -29,8 +29,8 @@ load_dotenv()
 
 def init():
     st.session_state['init']=True
-    st.session_state['llm']=ChatOpenAI()
-    # st.session_state['llm']=ChatOpenAI(model_name='gpt-4-turbo-preview')
+    # st.session_state['llm']=ChatOpenAI()
+    st.session_state['llm']=ChatOpenAI(model_name='gpt-4-turbo-preview')
     st.session_state['pandas_llm']=pandas_openai()
     st.session_state['pdf_file']=None
     st.session_state['csv_file']=None
@@ -116,7 +116,15 @@ def handel_audio():
     return transcription
 def handel_csv():
     st.session_state['smart_df']=SmartDataframe(pd.read_csv(st.session_state.csv_file),config={'llm':st.session_state.pandas_llm})
+
+def handel_url():
+    if st.session_state.website_url is None:
+        st.error("Please enter a website url")
+        return
     
+    loader = WebBaseLoader(st.session_state.website_url)
+    document = loader.load()
+    st.session_state['website_content']=document
 
 def get_response(query):
     if st.session_state.app=="Chat with PDF":
@@ -129,8 +137,13 @@ def get_response(query):
         st.session_state.transcription=transcription
         return response.content
     elif st.session_state.app=="Chat with CSV":
+        handel_csv()
         response= st.session_state.smart_df.chat(query)
         return response
+    elif st.session_state.app=="Chat with Website":
+        handel_url()
+        response=st.session_state.llm.invoke(f"base your answer on the following context {st.session_state['website_content']} and answer the following query: {query}")
+        return response.content
 
 
 
@@ -145,7 +158,11 @@ def main():
     #interaction stage
 
     user_input=st.chat_input("Ask me Anything...")
+    
+    
     if user_input:
+        
+        
         if st.session_state.app=="Chat with PDF":
             if st.session_state.pdf_file is None:
                 st.session_state.chat_history.append(AIMessage('Please upload a pdf file'))
@@ -154,8 +171,10 @@ def main():
                 response=get_response(user_input)
                 st.session_state.chat_history.append(HumanMessage(user_input))
                 st.session_state.chat_history.append(AIMessage(response))
-                # st.write(get_response(user_input))
+                
             
+        
+        
         elif st.session_state.app=="Chat with Audio":
             if st.session_state.audio_file is None:
                 st.session_state.chat_history.append(AIMessage('Please upload a audio file'))
@@ -166,11 +185,14 @@ def main():
                 st.session_state.chat_history.append(HumanMessage(user_input))
                 st.session_state.chat_history.append(AIMessage(response))
             
+        
+        
+        
         elif st.session_state.app=="Chat with CSV":
             if st.session_state.csv_file is None:
                 st.session_state.chat_history.append(AIMessage('Please upload a csv file'))
             else:
-                handel_csv()
+                
                 response=get_response(user_input)
                 
                 st.session_state.chat_history.append(HumanMessage(user_input))
@@ -179,15 +201,19 @@ def main():
                 except:
                     st.session_state.chat_history.append(AIMessage("here is the answer: ",additional_kwargs={"content":response}))
                 
+        
+        
+        
         elif st.session_state.app=="Chat with Website":
             if st.session_state.website_url is None:
                 st.session_state.chat_history.append(AIMessage('Please enter a website url'))
             else:
-                st.write("Chat with website")
-            #     response=get_response(user_input)
-            #     st.session_state.chat_history.append(HumanMessage(user_input))
-            #     st.session_state.chat_history.append(AIMessage(response))
-            #     display_chat_history()
+                
+                
+                response=get_response(user_input)
+                st.session_state.chat_history.append(HumanMessage(user_input))
+                st.session_state.chat_history.append(AIMessage(response))
+            
     display_chat_history()
 
 if __name__=="__main__":
